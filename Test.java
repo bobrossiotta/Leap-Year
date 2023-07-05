f9eb8d6786e67a2bde74f33bb154a2c99be07b83
bobrossiotta <137832556+bobrossiotta@users.noreply.github.com>
2023-07-05 22:54:07 +0300

    added all files

---
+++ Test.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+
+                Scanner inp = new Scanner(System.in);
+                int year;
+
+                System.out.print("Enter year: ");
+                year = inp.nextInt();
+
+                if (!(year%400 == 0)){
+                    if (!(year%100 == 0)){
+                        if(year%4 == 0){
+                            System.out.println("Leap year");
+                        }
+                        else
+                            System.out.println("Not a leap year");
+                    }
+                    else
+                        System.out.println("Not a leap year");
+                }
+                else
+                    System.out.println("Leap year");
+            }
+        }
